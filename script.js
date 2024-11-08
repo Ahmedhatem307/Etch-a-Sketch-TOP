c5a2cce93c4f070ad4c847c8b161080796fe31af
Ahmed <ahmedhatem.aez@gmail.com>
2024-11-08 15:48:27 +0200

    Add random colors and coloring while press the mouse

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const container = document.getElementById("container");
+const button = document.querySelector("#btn");
+let mouseDown = false
+
+//to keep tracking if mouse is kept pressed or not
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
 
 function makeGrid(num){
     if(num >= 100){
@@ -14,16 +21,29 @@ function makeGrid(num){
             for(let j = 0; j < num; j++){
                 const row = document.createElement("div");
                 row.classList.add("row");
+                row.addEventListener("mouseover",function(){
+                    //if the button is pressed the canvas is colored
+                    if(mouseDown)
+                    row.style.backgroundColor = randomColor();
+                });
                 col.appendChild(row);
             }
         }
     }
 }
 
-const button = document.querySelector("#btn");
 button.addEventListener("click",() => {
     let number = prompt("Please enter number of square per side (Max 100)");
     container.replaceChildren();
     makeGrid(number);
 });
 
+//generate a random color to be selected
+function randomColor() {
+    let color = [];
+
+    for(let i=0;i<3;i++){
+        color.push(Math.floor(Math.random()*256));
+    }
+    return 'rgb(' + color.join(',') + ')';
+}
